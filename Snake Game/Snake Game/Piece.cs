215843a6632bd2ba0592841c84b893f1be6d0eca
M A L E K  A L T A F <75058908+malekaltaf@users.noreply.github.com>
2021-03-20 16:17:35 +0530

    Initial commit

---
+++ Snake Game/Snake Game/Piece.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Game
+{
+    class Piece : Label
+    {
+        public Piece (int x, int y)
+        {
+            Location = new System.Drawing.Point(x, y);
+            Size = new System.Drawing.Size(20, 20);
+            BackColor=Color.Orange;
+            Enabled = false;
+        }
+    }
+}
---
+++ scrollbar/scrollbar/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace scrollbar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void moveclr(object sender, ScrollEventArgs e)
+        {
+            pictureBox1.BackColor = Color.FromArgb(hScrollBar1.Value, hScrollBar2.Value, hScrollBar3.Value);
+            label1.Text = hScrollBar1.Value.ToString();
+            label2.Text = hScrollBar2.Value.ToString();
+            label3.Text = hScrollBar3.Value.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(hScrollBar1.Value, hScrollBar2.Value, hScrollBar3.Value);
+        }
+    }
+}
